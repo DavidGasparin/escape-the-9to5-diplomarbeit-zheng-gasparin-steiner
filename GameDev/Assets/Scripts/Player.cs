d1139329772367916ee3b1242f45bacf986af2cd
Klaus263793 <220038@o365.htl-leoben.at>
2026-03-15 14:21:33 +0100

    implemented movement

--- GameDev/Assets/Scripts/Player.cs
+++ GameDev/Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     PlayerInput playerInput;
     InputAction move;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is createdd
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
