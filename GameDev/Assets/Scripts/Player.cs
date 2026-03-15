8eb6b93f0abeb89ff8cf3cea963ce3aa44ddb7aa
Klaus263793 <220038@o365.htl-leoben.at>
2026-03-15 14:39:10 +0100

    test push

--- GameDev/Assets/Scripts/Player.cs
+++ GameDev/Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     PlayerInput playerInput;
     InputAction move;
-    // Start is called once before the first execution of Update after the MonoBehaviour is createdd
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
