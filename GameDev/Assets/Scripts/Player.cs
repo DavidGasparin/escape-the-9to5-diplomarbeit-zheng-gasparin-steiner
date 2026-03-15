4002b06f955d6bbd38ed0aa82ae6dd6896176ce0
DavidGasparin <davidgasparin99@gmail.com>
2026-03-15 15:50:50 +0100

    optimized move/sprint logic

--- GameDev/Assets/Scripts/Player.cs
+++ GameDev/Assets/Scripts/Player.cs
@@ -1,52 +1,64 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
-    
+    //@Todo: Spieler soll langsamer werden wen gehen aufhört und nicht dierekt stoppen -> speed immer halbieren oder so
     [SerializeField] private float initialMovespeed = 5.0f;
     [SerializeField] private float sprintMulitiplyer = 1.5f;
+
+    private float sprintSpeed;
 
     private float movespeed;
     public Rigidbody rb;
     PlayerInput playerInput;
     InputAction move;
     InputAction sprint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is createdd
     void Start()
     {
-        this.movespeed = initialMovespeed;
+        caculatSpeeds();
         rb = this.GetComponent<Rigidbody>();
         playerInput = this.GetComponent<PlayerInput>();
         move= this.playerInput.actions.FindAction("Move");
         sprint = this.playerInput.actions.FindAction("Sprint");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckSprint();
-        MovePlayer();
-        this.movespeed = initialMovespeed;
-
+        if (sprint.IsPressed())
+        {
+            MovePlayer(sprintSpeed);
+        }
+        MovePlayer(initialMovespeed);
     }
-    void MovePlayer()
+
+    void MovePlayer(float speed)
     {
         Vector2 direction= move.ReadValue<Vector2>();
         Debug.Log(direction);
-        rb.transform.position+= new Vector3(direction.x, 0, direction.y) * movespeed * Time.deltaTime;
+        rb.transform.position+= new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
     }
 
-    void CheckSprint()
+    public void setInitialSpeed(float newSpeed)
     {
-        if (sprint.IsPressed())
-        {
-            this.movespeed *= sprintMulitiplyer;
-        }
-        else
-        {
-            movespeed  = initialMovespeed;
-        }
+        this.initialMovespeed = newSpeed;
+        caculatSpeeds();
+    }
+
+    public void setSprintMultiplyer(float newMultiplyer)
+    {
+        this.sprintMulitiplyer = newMultiplyer;
+        caculatSpeeds();
+    }
+     private void caculatSpeeds()
+    {
+        this.movespeed = initialMovespeed;
+        this.sprintSpeed = initialMovespeed * sprintMulitiplyer;
     }
 }
