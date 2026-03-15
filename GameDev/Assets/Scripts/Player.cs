0c47f044694c449fa2d459179eda73fa97efcdfe
Klaus263793 <220038@o365.htl-leoben.at>
2026-03-15 14:16:10 +0100

    implemented input action-system

--- GameDev/Assets/Scripts/Player.cs
+++ GameDev/Assets/Scripts/Player.cs
@@ -1,18 +1,30 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int player = 0;
+    [SerializeField] private float movespeed = 5.0f;
+    public Rigidbody rb;
+    PlayerInput playerInput;
+    InputAction move;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        rb = this.GetComponent<Rigidbody>();
+        playerInput = this.GetComponent<PlayerInput>();
+        move= this.playerInput.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        Vector2 direction= move.ReadValue<Vector2>();
+        Debug.Log(direction);
+        rb.transform.position+= new Vector3(direction.x, 0, direction.y) * movespeed * Time.deltaTime;
     }
 }
